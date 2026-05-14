1385b4add85978224b47de54bae9aaf2d627e871
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-15 00:44:21 +0530

    Roboshop Project

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,3 +1,5 @@
 variable "component" {}
 variable "vm_size" {}
 variable "env" {}
+
+variable "vault_token" {}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,5 @@ variable "env" {}
 variable "components" {}
 
 variable "token" {}
+
+variable "vault_token" {}
