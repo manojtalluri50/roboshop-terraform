396490706d87cbd03ce105c024fa31c940bbb9c6
Talluri Manoj Kumar <87233317+manojtalluri50@users.noreply.github.com>
2026-05-23 20:47:39 +0530

    Roboshop Project

---
+++ modules/aks/main.tf
@@ -0,0 +1,17 @@
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = "main"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  kubernetes_version = "1.31.2"
+  dns_prefix          = "dev"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
